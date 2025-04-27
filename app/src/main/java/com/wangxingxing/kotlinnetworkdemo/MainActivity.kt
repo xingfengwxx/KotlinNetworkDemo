20b8f22b7ff277b1204100b2c2f359ef8024ce1e
WangXingxing <1099420259@qq.com>
2025-04-27 20:27:22 +0800

    修改显示

--- app/src/main/java/com/wangxingxing/kotlinnetworkdemo/MainActivity.kt
+++ app/src/main/java/com/wangxingxing/kotlinnetworkdemo/MainActivity.kt
@@ -1,67 +1,43 @@
 package com.wangxingxing.kotlinnetworkdemo
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import com.wangxingxing.kotlinnetworkdemo.base.BaseViewModelActivity
 import com.wangxingxing.kotlinnetworkdemo.databinding.ActivityMainBinding
 import com.wangxingxing.kotlinnetworkdemo.viewmodel.MainViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseViewModelActivity<MainViewModel>(R.layout.activity_main) {
 
-    private val mBinding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
-
-    private val mViewModel by viewModels<MainViewModel>()
-
+    private var tvNetResult: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(mBinding.root)
+
+        tvNetResult = findViewById(R.id.tv_net_result)
 
         init()
         initObserver()
     }
 
     private fun init() {
-        mViewModel.getHomeList(1)
+        viewModel.getHomeList(1)
     }
 
     private fun initObserver() {
         lifecycleScope.launchWhenStarted {
-            mViewModel.list.collect() {
-                mBinding.tvNetResult.text = it.toString()
+            viewModel.list.collect() {
+                tvNetResult?.text = it?.data?.datas.toString()
             }
         }
-    }
-
-//    private fun showLoading() {
-//        //避免接口请求返回时间太短，出现loading闪烁问题。500ms以内不会显示loading，显示loading后至少显示500ms。
-//        mBinding.loadingProgressBar.show()
-//    }
-//
-//    private fun dismissLoading() {
-//        mBinding.loadingProgressBar.hide()
-//    }
-
-    private fun showNetErrorPic(isShowError: Boolean) {
-        mBinding.tvNetResult.isGone = isShowError
-        mBinding.aivError.isVisible = isShowError
     }
 
     companion object {
         const val TAG = "wxx"
     }
 
-//    override fun showLoading() {
-//        //避免接口请求返回时间太短，出现loading闪烁问题。500ms以内不会显示loading，显示loading后至少显示500ms。
-//        mBinding.loadingProgressBar.show()
-//    }
-//
-//    override fun dismissLoading() {
-//        mBinding.loadingProgressBar.hide()
-//    }
 }
